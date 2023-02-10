c81f5dfd0298330c762376c9c61c639217dc5aaf
rajneesh44 <rajneeshsharma338@gmail.com>
2023-02-11 02:14:29 +0530

    added long ttl for cookie with permanent session

--- icc_session_interface.py
+++ icc_session_interface.py
@@ -1,9 +1,11 @@
 import os
-from flask import session, request
+import hashlib
 from werkzeug.datastructures import CallbackDict
 from flask.sessions import SessionMixin, SecureCookieSessionInterface
 from itsdangerous import URLSafeTimedSerializer, BadSignature
 
+
+COOKIE_TTL = int(os.getenv("COOKIE_EXPIRATION_TIME", 0))
 
 class ICCSession(CallbackDict, SessionMixin):
     def __init__(self, initial=None):
@@ -13,23 +15,33 @@ class ICCSession(CallbackDict, SessionMixin):
         self.modified = False
 
 class ICCSessionInterface(SecureCookieSessionInterface):
-    salt = os.getenv('SESSION_SALT')
+    salt = "cookie-session"
     session_class = ICCSession
 
+    digest_method = staticmethod(hashlib.sha512)
+    key_derivation = "hmac"
+
     def get_serializer(self, app):
+        signer_kwargs = dict(
+            key_derivation=self.key_derivation, digest_method=self.digest_method
+        )
         if not app.secret_key:
             return None
-        return URLSafeTimedSerializer(app.secret_key,
-                                      salt=self.salt)
+        return URLSafeTimedSerializer(
+            app.secret_key,
+            salt=self.salt,
+            signer_kwargs=signer_kwargs
+        )
     
     def open_session(self, app, request):
+        app.permanent_session_lifetime = COOKIE_TTL
         csession = self.get_serializer(app)
         if csession is None:
             return None
         val = request.cookies.get(app.session_cookie_name)
         if not val:
             return self.session_class()
-        max_age = 60*60*24*30        
+        max_age = COOKIE_TTL
         try:
             data = csession.loads(val, max_age=max_age)
             return self.session_class(data)
@@ -44,6 +56,7 @@ class ICCSessionInterface(SecureCookieSessionInterface):
                 response.delete_cookie(app.session_cookie_name,
                                    domain=domain)
             return
+        session.permanent = True
         expires = self.get_expiration_time(app, session)
         val = self.get_serializer(app).dumps(dict(session))
         response.set_cookie(app.session_cookie_name, val,
@@ -51,3 +64,5 @@ class ICCSessionInterface(SecureCookieSessionInterface):
                             domain=domain)
 
   
+
+# CHECK LOGOUT FLOW OF COOKIES
