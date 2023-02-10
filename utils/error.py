15690f20c492827b12859999224c5d73e4643ecd
rajneesh44 <rajneeshsharma338@gmail.com>
2023-02-11 01:56:04 +0530

    added custom response and error class

--- utils/error.py
+++ utils/error.py
@@ -1,2 +1,12 @@
-class CustomErrorTemplates:
-    UNAUTHORIZED = (404, "Unauthorized")
+from enum import Enum
+
+class ICCError(Enum):
+    def __init__(self, error_code, error_message):
+        super().__init__()
+        self.error_code = error_code
+        self.error_message = error_message
+
+
+
+class CustomICCError(ICCError):
+    UNAUTHORIZED = (401, "Unauthorized")
